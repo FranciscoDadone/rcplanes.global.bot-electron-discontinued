4b71c8dee73d5d7480839f16fd1c5dd6e7465731
FranciscoDadone <dadonefran@gmail.com>
2022-02-20 23:16:06 -0300

    made hashtag fetching in intervals of 10 minutes.

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,13 @@ const { ipcRenderer } = require('electron')
 
 
 function a() {
-
   ipcRenderer.send('anything-asynchronous', 'ping')
-
 }
 
 function App() {
   return (
     <div>
-      <h1>Hello, Electron!</h1>
-
-      <button onClick={a}>Hello</button>
+      
 
     </div>
   )
